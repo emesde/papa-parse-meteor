ec583926b01525228d29fabd2d6c969a78627617
David Nguyen <me@davidn.win>
2018-10-30 13:00:50 -0700

    upgrade papa-parse to 4.6, remove baby-parse

--- package.js
+++ package.js
@@ -2,21 +2,19 @@ Package.describe({
   name: 'harrison:papa-parse',
   summary: 'A powerful, in-browser CSV parser for big boys and girls',
   git: 'https://github.com/harrisonhunter/papa-parse-meteor.git',
-  version: '1.1.3'
+  version: '1.1.4'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("METEOR@0.9.3");
   api.use("jquery");
-  api.addFiles('papa-parse.js', 'client');
-  api.addFiles('baby-parse.js', 'server');
+  api.addFiles('papa-parse.js', ['client', 'server']);
   api.export('Papa', ['server', 'client']);
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('harrison:papa-parse');
-  api.addFiles('papa-parse-tests.js', 'client');
-  api.addFiles('baby-parse-tests.js', 'server');
+  api.addFiles('papa-parse-tests.js', ['client', 'server']);
   api.export('Papa',  ['server', 'client']);
 });
